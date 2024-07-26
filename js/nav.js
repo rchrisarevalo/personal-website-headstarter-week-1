8f22bc747c3f50117423dda427d447ce20349e2e
rchrisarevalo <rubenplayer105@gmail.com>
2024-07-26 13:21:31 -0500

    Pushing final changes

---
+++ js/nav.js
@@ -0,0 +1,41 @@
+const toggleNav = () => {
+    const menu = document.getElementById("nav-menu")
+    const menuLink = document.getElementById("menu-link")
+    const closeBtn = document.getElementById("mobile-nav-menu-close")
+    const openBtn = document.getElementById("mobile-nav-menu-open")
+
+    openBtn.addEventListener('click', () => {
+        menu.style.right = '0%'
+        menu.style.transition = '.5s'
+        document.body.style.overflow = 'hidden'
+
+        closeBtn.style.right = '0%'
+        closeBtn.style.transition = '.5s'
+    })
+
+    closeBtn.addEventListener('click', () => {
+        menu.style.right = '-100%'
+        menu.style.transition = '.5s'
+        document.body.style.overflow = 'auto'
+
+        closeBtn.style.right = '-100%'
+        closeBtn.style.transition = '.5s'
+    })
+
+
+    // if (action === "open") {
+    //     menu.style.right = '0%'
+    //     menu.style.transition = '.5s'
+    //     document.body.style.overflow = 'hidden'
+
+    //     closeBtn.style.right = '0%'
+    //     closeBtn.style.transition = '.5s'
+    // } else {
+    //     menu.style.right = '-100%'
+    //     menu.style.transition = '.5s'
+    //     document.body.style.overflow = 'auto'
+
+    //     closeBtn.style.right = '-100%'
+    //     closeBtn.style.transition = '.5s'
+    // }
+}
